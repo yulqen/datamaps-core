c9713a6bf6facab8d91f0eab6055b6aad7741818
Matthew Lemon <lemon@matthewlemon.com>
2019-03-19 21:06:54 +0000

    inital commit

---
+++ src/test/java/parser/XLSXParserTest.java
@@ -0,0 +1,59 @@
+package parser;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class XLSXParserTest {
+
+    private File testFile;
+    private final String sheetName = "Sheet1";
+
+    @Before
+    public void setUp() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        testFile = new File(classLoader.getResource("files/test.xlsx").getFile());
+        Workbook workbook = new XSSFWorkbook();
+        OutputStream fileOutputStream = new FileOutputStream("/home/lemon/Desktop/test.xlsx");
+        Sheet sheet = workbook.createSheet("new sheet");
+
+        Row row1 = sheet.createRow(0);
+        Cell r1c1 = row1.createCell(0);
+        r1c1.setCellValue("Project/Programme Name");
+
+        Cell r1c2 = row1.createCell(1);
+        r1c2.setCellValue("Crumpton Colliery");
+
+        workbook.write(fileOutputStream);
+        fileOutputStream.close();
+        System.out.println("Done");
+    }
+
+    @Test
+    public void getCellValueWithCellReference() throws IOException, InvalidFormatException {
+        XLSXSheetData sheetData = new XLSXSheetData(testFile, sheetName);
+        Map dataFromSheet = sheetData.getDataMapHash();
+        assertEquals(dataFromSheet.get(new CellReference("B1")), "Useless Project");
+    }
+
+    @Test
+    public void getCellValueWithString() throws IOException, InvalidFormatException {
+        XLSXSheetData sheetData = new XLSXSheetData(testFile, sheetName);
+        Map dataFromSheet = sheetData.getDataMapHash_StringsAsKeys();
+        assertEquals(dataFromSheet.get("B1"), "Useless Project");
+    }
+}
