30462080467255c0f108bf1537d7ae84145ad742
Matthew Lemon <lemon@matthewlemon.com>
2019-03-26 20:22:25 +0000

    doing some refactoring

--- src/test/java/com/matthewlemon/dbasiktest/usecases/CreateableDatamapUseCaseShould.java
+++ src/test/java/com/matthewlemon/dbasiktest/usecases/CreateableDatamapUseCaseShould.java
@@ -3,39 +3,48 @@ package com.matthewlemon.dbasiktest.usecases;
 import com.matthewlemon.dbasiktest.Context;
 import com.matthewlemon.dbasiktest.TestSetup;
 import com.matthewlemon.dbasiktest.entities.Datamap;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class CreateableDatamapUseCaseShould {
+
+    private CreateableDatamapUseCase useCase;
+    private Datamap testDatamap;
+    private String TITLE;
 
     @Before
     public void setUp() {
         TestSetup.setupContext();
+
+        TITLE = "Test Datamap";
+
+        useCase = new CreateableDatamapUseCase();
     }
 
     @Test
-    public void createDatamap() {
-        CreateableDatamapUseCase useCase = new CreateableDatamapUseCase();
-        assertThat(useCase.createDatamap("Test Datamap").getName(), is("Test Datamap"));
+    public void datamapIsCreated() {
+        testDatamap = useCase.createDatamap(TITLE);
+        assertThat((testDatamap).getName(), is(TITLE));
     }
 
     @Test
     public void canGetCountOfLinesInDatamap() {
-        CreateableDatamapUseCase useCase = new CreateableDatamapUseCase();
-        useCase.createDatamap("Test Datamap");
-        useCase.addLineToDatamap("Test Datamap", "Key 1", "Sheet 1", "A2");
-        assertThat(useCase.getLineCountFromDatamap("Test Datamap"), is(1));
+        testDatamap = useCase.createDatamap(TITLE);
+        useCase.addLineToDatamap(TITLE, "Key 1", "Sheet 1", "A2");
+        int lineCount = useCase.getLineCountFromDatamap(TITLE);
+        assertThat(lineCount, is(1));
     }
 
     @Test
     public void findDatamapWithName() {
-        CreateableDatamapUseCase useCase = new CreateableDatamapUseCase();
-        useCase.createDatamap("Test Datamap");
+        testDatamap = useCase.createDatamap(TITLE);
         Datamap returnedDatamap = Context.datamapGateway.getDatamapWithName("Test Datamap");
-        assertThat(returnedDatamap.getName(), is("Test Datamap"));
+        assertThat(returnedDatamap.getName(), is(TITLE));
     }
 
 }
