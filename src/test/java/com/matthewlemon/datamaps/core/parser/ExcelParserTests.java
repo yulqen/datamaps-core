658ee3549cc38c04f7ff0126435ecb8e806ac2ff
Matthew Lemon <lemon@matthewlemon.com>
2019-04-11 19:52:24 +0100

    started to test new DatamapType set up from spike

---
+++ src/test/java/com/matthewlemon/datamaps/core/parser/ExcelParserTests.java
@@ -0,0 +1,20 @@
+package com.matthewlemon.datamaps.core.parser;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExcelParserTests {
+
+	private static final String TEST_FILE_XLSX = "/home/lemon/Desktop/test_populated_template.xlsx";
+	
+	@Before
+	public void setUp() {
+		File testFile = new File(TEST_FILE_XLSX);
+	}
+	
+	@Test
+	public void parseSpreadsheetAndReturnExpectedValues() throws Exception {
+	}
+}
