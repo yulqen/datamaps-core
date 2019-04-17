9565d88669f1d9ffa5da871ecb2ebd51f9a18f91
Matthew Lemon <lemon@matthewlemon.com>
2019-04-17 21:18:39 +0100

    some cleanup

--- src/test/java/com/matthewlemon/datamaps/core/gateway/InMemoryDatamapShould.java
+++ src/test/java/com/matthewlemon/datamaps/core/gateway/InMemoryDatamapShould.java
@@ -1,10 +1,9 @@
 package com.matthewlemon.datamaps.core.gateway;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
@@ -26,71 +25,68 @@ import com.matthewlemon.datamaps.core.parser.DatamapType;
 
 public class InMemoryDatamapShould {
 
-    private static InMemoryDatamapGateway gateway;
-    private static File testFile;
-    private static CSVFile csvFile;
+	private static InMemoryDatamapGateway gateway;
+	private static File testFile;
+	private static CSVFile csvFile;
 
-    @BeforeClass
-    public static void setUpClass() {
-        TestSetup.setupContext();
-    }
+	@BeforeClass
+	public static void setUpClass() {
+		TestSetup.setupContext();
+	}
 
-    @Before
-    public void setUp() throws DuplicateDatamapException {
-    	ClassLoader classLoader = getClass().getClassLoader();
-        testFile = new File(classLoader.getResource("files/test.csv")
-        		.getFile());
-        csvFile = new CSVFile(testFile);
-        gateway = new InMemoryDatamapGateway();
-        gateway.createDatamap("Test Datamap");
-    }
+	@Before
+	public void setUp() throws DuplicateDatamapException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		testFile = new File(classLoader.getResource("files/test.csv").getFile());
+		csvFile = new CSVFile(testFile);
+		gateway = new InMemoryDatamapGateway();
+		gateway.createDatamap("Test Datamap");
+	}
 
-    @After
-    public void clearDatamapLines() {
-        gateway.deleteAllLinesIn("Test Datamap");
-    }
+	@After
+	public void clearDatamapLines() {
+		gateway.deleteAllLinesIn("Test Datamap");
+	}
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-    
-    @Test
-    public void addLineToDatamap() throws DatamapNotFoundException {
-        gateway.addLineToDatamap("Test Datamap",
-                "Test Key", "Sheet 1", "B1", new DatamapType());
-        List<DatamapLine> datamaplines = gateway.getDataLinesFor("Test Datamap");
-        assertEquals("Test Key", datamaplines.get(0).getKey());
-        // manual delete of that line
-    }
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
-    public void useGatewayToAddToDatamapWithCSV() throws DatamapNotFoundException {
-        DatamapLine dml = new DatamapLine("Test Key 1", "Test Sheet 1",
-                "Test CellRef 1", new DatamapType());
-        gateway.addDataToDatamapWithCSV("Test Datamap", csvFile);
-        assertEquals(dml.getKey(), gateway.getDatamap("Test Datamap")
-                .getDatamapLines().get(0).getKey());
-    }
+	public void addLineToDatamap() throws DatamapNotFoundException {
+		gateway.addLineToDatamap("Test Datamap", "Test Key", "Sheet 1", "B1", new DatamapType());
+		List<DatamapLine> datamaplines = gateway.getDataLinesFor("Test Datamap");
+		assertEquals("Test Key", datamaplines.get(0).getKey());
+		// manual delete of that line
+	}
 
-    @Test
-    public void addDataToDatamapFromCSV() throws IOException, DatamapNotFoundException {
-        Datamap datamap = gateway.getDatamap("Test Datamap");
-        datamap.readCSV(testFile);
-        assertEquals("Test Key 1", datamap.getDataKeyForLine(0));
-        assertEquals("Test Key 2", datamap.getDataKeyForLine(1));
-    }
-    
-    @Test(expected=DuplicateDatamapException.class)
+	@Test
+	public void useGatewayToAddToDatamapWithCSV() throws DatamapNotFoundException {
+		DatamapLine dml = new DatamapLine("Test Key 1", "Test Sheet 1", "Test CellRef 1", new DatamapType());
+		gateway.addDataToDatamapWithCSV("Test Datamap", csvFile);
+		assertEquals(dml.getKey(), gateway.getDatamap("Test Datamap").getDatamapLines().get(0).getKey());
+	}
+
+	@Test
+	public void addDataToDatamapFromCSV() throws IOException, DatamapNotFoundException {
+		Datamap datamap = gateway.getDatamap("Test Datamap");
+		datamap.readCSV(testFile);
+		assertEquals("Test Key 1", datamap.getDataKeyForLine(0));
+		assertEquals("Test Key 2", datamap.getDataKeyForLine(1));
+	}
+
+	@Test(expected = DuplicateDatamapException.class)
 	public void rejectDatamapWithSameName() throws Exception {
 		@SuppressWarnings("unused")
 		Datamap datamap = gateway.createDatamap("Test Datamap");
 		@SuppressWarnings("unused")
 		Datamap datamap2 = gateway.createDatamap("Test Datamap");
 	}
-    
-    @Test
-	public void canGetSingleDatamapLineByQueryingItsSheetAndKeyValues() throws DuplicateDatamapException, DatamapNotFoundException, DatamapLineNotFoundException {
-    	@SuppressWarnings("unused")
-        Datamap datamap = gateway.createDatamap("Test Datamap 2"); // reate a new Datamap object
+
+	@Test
+	public void canGetSingleDatamapLineByQueryingItsSheetAndKeyValues()
+			throws DuplicateDatamapException, DatamapNotFoundException, DatamapLineNotFoundException {
+		@SuppressWarnings("unused")
+		Datamap datamap = gateway.createDatamap("Test Datamap 2"); // create a new Datamap object
 		gateway.addLineToDatamap("Test Datamap 2", "Test Key 1", "Test Sheet 1", "B1");
 		gateway.addLineToDatamap("Test Datamap 2", "Test Key 2", "Test Sheet 1", "B2");
 		DatamapLine dml = gateway.getDatamapLineFrom("Test Datamap 2", "Test Key 1");
@@ -99,11 +95,12 @@ public class InMemoryDatamapShould {
 		assertEquals("B1", dml.getCellRef());
 	}
 
-    @Test
+	@Test
 	public void throwExceptionWhenDatamapDoesntExist() throws Exception {
-    	thrown.expect(DatamapNotFoundException.class);
-    	thrown.expectMessage("Datamap Test Datamap No Exist cannot be found.");
-    	Datamap datamap = gateway.getDatamap("Test Datamap No Exist");
+		thrown.expect(DatamapNotFoundException.class);
+		thrown.expectMessage("Datamap Test Datamap No Exist cannot be found.");
+		@SuppressWarnings("unused")
+		Datamap datamap = gateway.getDatamap("Test Datamap No Exist");
 	}
 
 }
