cd0b9939aff5de8e259e54b5c867786cef26749d
Matthew Lemon <lemon@matthewlemon.com>
2020-09-20 20:55:10 +0100

    Moved a bunch of files in from Spring Initializr

---
+++ src/test/java/com/matthewlemon/datamaps/core/DatamapsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewlemon.datamaps.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamapsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
